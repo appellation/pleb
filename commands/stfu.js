4f3eab53e4e173296c3ff2a0be51a02cab0cdfee
appellation <nelson.will@live.com>
2016-08-27 09:27:13 -0700

    Fixes #17.

--- commands/stfu.js
+++ commands/stfu.js
@@ -1,8 +1,17 @@
 /**
  * Created by Will on 8/25/2016.
+ *
+ * @param {Client} client
+ * @param {Message} msg
+ * @param {[]} args
  */
 function Stfu(client, msg, args)    {
-    client.voiceConnections.get('server', msg.server).destroy();
+    const vc = client.voiceConnections.get('server', msg.server);
+    if(vc)  {
+        vc.destroy();
+    }   else    {
+        msg.reply('when someone asks you to do something and you\'ve already done it. :joy:');
+    }
 }
 
 module.exports = Stfu;
