205a92801d2da964a37faf2042108af450ad1b51
appellation <nelson.will@live.com>
2016-09-07 12:33:07 -0700

    Update for new playlist key.

--- commands/resume.js
+++ commands/resume.js
@@ -2,7 +2,7 @@
  * Created by Will on 8/30/2016.
  */
 function Resume(client, msg, args)  {
-    const playlist = msg.server.ytPlaylist;
+    const playlist = msg.server.playlist;
     if(playlist)    {
         playlist.resume();
     }   else    {
