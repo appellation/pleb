bfaae90a19f713de6269628610f782b4c83a390c
appellation <nelson.will@live.com>
2016-09-12 13:08:31 -0700

    Remove ping package.

--- commands/ping.js
+++ commands/ping.js
@@ -2,12 +2,10 @@
  * Created by Will on 9/12/2016.
  */
 
+const validURL = require('valid-url');
+
 function Ping(client, msg, args)    {
-    if(!args[0])    {
-        msg.channel.sendMessage('pinging....').then(function(newMessage) {
-            newMessage.edit('it took ' + (newMessage.timestamp.getTime() - msg.timestamp.getTime()) + 'ms to send this message.');
-        });
-    }
+    //
 }
 
 module.exports = Ping;
