b08cb7e24351a47492b43edf242561db70393714
appellation <nelson.will@live.com>
2016-10-15 11:41:01 -0700

    Fix inefficient map->array conversion.

--- commands/stats.js
+++ commands/stats.js
@@ -8,13 +8,13 @@ function Status(client, msg, args)  {
     let reply;
     if(args[0] === 'guilds' && msg.author.id == '116690352584392704')    {
         reply = client.guilds.map(function(guild)    {
-                return (guild.available ? ":white_check_mark:" : ":x") + " **" + guild.name + "** - `" + guild.members.array().length + "` members - `" + guild.owner.user.username + "#" + guild.owner.user.discriminator + "`";
+                return (guild.available ? ":white_check_mark:" : ":x:") + " **" + guild.name + "** - `" + guild.members.size + "` members - `" + guild.owner.user.username + "#" + guild.owner.user.discriminator + "`";
             }).join("\n");
     }   else    {
-        reply = "**Guilds:** " + client.guilds.array().length + "\n" +
-            "**Channels:** " + client.channels.array().length + "\n" +
+        reply = "**Guilds:** " + client.guilds.size + "\n" +
+            "**Channels:** " + client.channels.size + "\n" +
             "**Start time:** " + moment(client.readyTime).format("MMMM D, YYYY, h:mm:ss a") + "\n" +
-            "**Users:** " + client.users.array().length;
+            "**Users:** " + client.users.size;
     }
 
     msg.channel.sendMessage(reply);
