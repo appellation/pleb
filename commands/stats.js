01612c10a991d3d451b64bb58abd11d3bbf237ff
appellation <nelson.will@live.com>
2016-10-18 10:50:58 -0700

    Fix stats?

--- commands/stats.js
+++ commands/stats.js
@@ -5,19 +5,18 @@
 const moment = require('moment');
 
 function Status(client, msg, args)  {
-    let reply;
-    if(args[0] === 'guilds' && msg.author.id == '116690352584392704')    {
-        reply = client.guilds.map(function(guild)    {
+    return new Promise(resolve => {
+        if(args[0] === 'guilds' && msg.author.id == '116690352584392704')    {
+            resolve(client.guilds.map(function(guild)    {
                 return (guild.available ? ":white_check_mark:" : ":x:") + " **" + guild.name + "** - `" + guild.members.size + "` members - `" + guild.owner.user.username + "#" + guild.owner.user.discriminator + "`";
-            }).join("\n");
-    }   else    {
-        reply = "**Guilds:** " + client.guilds.size + "\n" +
-            "**Channels:** " + client.channels.size + "\n" +
-            "**Start time:** " + moment(client.readyTime).format("MMMM D, YYYY, h:mm:ss a") + "\n" +
-            "**Users:** " + client.users.size;
-    }
-
-    return Promise.resolve(reply);
+            }).join("\n"));
+        }   else    {
+            resolve("**Guilds:** " + client.guilds.size + "\n" +
+                "**Channels:** " + client.channels.size + "\n" +
+                "**Start time:** " + moment(client.readyTime).format("MMMM D, YYYY, h:mm:ss a") + "\n" +
+                "**Users:** " + client.users.size);
+        }
+    })
 }
 
 module.exports = Status;
