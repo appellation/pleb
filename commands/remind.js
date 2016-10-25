0636eba03328a968cfa8a195ca368e27cfdff9cc
Will Nelson <nelson.will@live.com>
2016-10-25 08:27:37 -0700

    Untested reminders.

--- commands/remind.js
+++ commands/remind.js
@@ -23,6 +23,17 @@ function Remind(client, msg, args)  {
     if(!remIndex || !timeIndex || timeIndex == -1 || remIndex == -1 || timeIndex < remIndex)    {
         return Promise.resolve('can\'t parse that :cry:');
     }
+
+    const date = moment(args.slice(timeIndex + 1));
+    if(!date.isValid() || date < moment()) {
+        return Promise.resolve('that date doesn\'t seem to be valid.');
+    }
+
+    schedule.scheduleJob(date, () => {
+        msg.reply(args.slice(remIndex + 1, timeIndex));
+    });
+
+    Promise.resolve('reminder set for ' + date.format('MMMM Do, YYYY HH:mm:ssZ a'));
 }
 
 module.exports = Remind;
