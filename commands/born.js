e6e77e7d9bbabbea02d20d3733403ab80048eeed
appellation <nelson.will@live.com>
2016-10-27 22:33:04 -0700

    fix v10 issue with borndates

--- commands/born.js
+++ commands/born.js
@@ -5,7 +5,7 @@
 const moment = require('moment');
 
 function Born(client, msg, args)    {
-    const date = msg.author.creationDate;
+    const date = msg.author.createdTimestamp;
     msg.reply("you were born on " + moment(date).format("MMMM Do, YYYY") + " at " + moment(date).format("h:mm:ss a"));
     return Promise.resolve();
 }
