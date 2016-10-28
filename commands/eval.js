0d89bbbba5b0af202de06daed03dbbef594ca0fe
appellation <nelson.will@live.com>
2016-10-27 21:09:02 -0700

    make split not retarded

--- commands/eval.js
+++ commands/eval.js
@@ -9,9 +9,7 @@ function Eval(client, msg, args)    {
             const res = eval(matches ? matches[1] : args.join(' '));
             if(typeof res != 'string')  {
                 const inspect = require('util').inspect(res);
-                for(let i = 0; i < inspect.length; i+= 1950)    {
-                    msg.channel.sendCode(inspect.slice(i, i + 1950))
-                }
+                msg.channel.sendCode("x1", inspect, {split: true});
             }   else {
                 msg.channel.sendCode("x1", res);
             }
