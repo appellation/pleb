0663381a225be61f3f355baced7316ef4617a599
appellation <nelson.will@live.com>
2016-10-27 22:18:13 -0700

    make eval better

--- commands/eval.js
+++ commands/eval.js
@@ -4,18 +4,18 @@
 
 function Eval(client, msg, args)    {
     if(msg.author.id == '116690352584392704' || msg.author.id == '104007599384449024')   {
-        try {
-            const matches = msg.content.match(/`(.*)`/);
-            const res = eval(matches ? matches[1] : args.join(' '));
-            if(typeof res != 'string')  {
-                const inspect = require('util').inspect(res);
-                msg.channel.sendCode("x1", inspect, {split: true});
-            }   else {
-                msg.channel.sendCode("x1", res, {split: true});
+
+        return new Promise(resolve => {
+            try {
+                const matches = msg.content.match(/`(.*)`/);
+                const res = eval(matches ? matches[1] : args.join(' '));
+                resolve(res);
+            }   catch (e)   {
+                resolve(e);
             }
-        }   catch (e)   {
-            msg.channel.sendCode("x1", e, {split: true});
-        }
+        }).then(require('util').inspect).then(res => {
+            msg.channel.sendCode("x1", res, {split: true});
+        })
     }
 }
 
