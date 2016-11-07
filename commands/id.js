723a0158565c64a91c86c7b0f6ec56f5844fe482
Will Nelson <nelson.will@live.com>
2016-11-07 08:20:03 -0800

    enhanced id fetching

--- commands/id.js
+++ commands/id.js
@@ -6,9 +6,25 @@
  * @param {Client} client
  * @param {Message} msg
  * @param {[]} args
+ * @return {string}
  */
 function Id(client, msg, args)  {
-    msg.author.sendMessage('`' + msg.author.id + '`');
+    const mentions = msg.mentions;
+
+    if(mentions.size == 0)  {
+        return '`' + msg.author.id + '`';
+    }
+
+    let out = [];
+    for(const channel of mentions.channels) {
+        out.push(channel[1] + ': `' + channel[1].id +'`');
+    }
+
+    for(const role of mentions.roles)   {
+        out.push(role[1] + ': `' + role[1].id + '`');
+    }
+
+    return out.join('\n');
 }
 
 module.exports = Id;
