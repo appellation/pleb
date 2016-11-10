18a86ed5174739db1370da3b9d08914281b82549
appellation <nelson.will@live.com>
2016-11-09 21:43:08 -0800

    morse

--- operators/command.js
+++ operators/command.js
@@ -155,6 +155,10 @@ class Command   {
             [
                 'weather',
                 require('../commands/weather')
+            ],
+            [
+                'morse',
+                require('../commands/morse')
             ]
         ];
 
