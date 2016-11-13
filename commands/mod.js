178cfe02b84725e939583659230f9620b75ae09e
Will Nelson <nelson.will@live.com>
2016-11-13 13:39:06 -0800

    start modding

---
+++ commands/mod.js
@@ -0,0 +1,14 @@
+/**
+ * Created by nelso on 11/13/2016.
+ */
+
+/**
+ * @param {Client} client
+ * @param {Message} msg
+ * @param {[]} args
+ */
+function Mod(client, msg, args) {
+    //
+}
+
+module.exports = Mod;
