39f0553a5cf39fb52083733a5bdc1d9e41ce13f9
appellation <nelson.will@live.com>
2016-12-03 22:21:09 -0800

    fix shuffle

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -13,12 +13,17 @@ const Play = require('./play');
 function Shuffle(client, msg, args) {
 
     if(args.length > 0) {
-        return Play(client, msg, args, null, true);
+        return Play(client, msg, args, {
+            shuffle: true
+        });
     }   else    {
         const playlist = msg.guild.playlist;
 
         if(playlist && playlist.list.list.length > 0)    {
-            return Play(client, msg, args, playlist, true);
+            return Play(client, msg, args, {
+                playlistIn: playlist,
+                shuffle: true
+            });
         }   else    {
             return msg.reply('takes two (or more :thinking:) to tango.');
         }
