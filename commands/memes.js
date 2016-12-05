6da4b2ebc36bcc82d046be68da46713bc1a060ad
appellation <nelson.will@live.com>
2016-12-04 20:13:04 -0800

    this code quality tho.

--- commands/memes.js
+++ commands/memes.js
@@ -2,7 +2,7 @@
  * Created by Will on 9/23/2016.
  */
 
-const rp = require('request-promise-native');
+const moment = require('moment');
 
 /**
  * @param {Client} client
@@ -10,27 +10,22 @@ const rp = require('request-promise-native');
  * @param {[]} args
  * @return {Promise|undefined}
  */
-function Memes(client, msg, args)
-{
-    var moment = require('moment');
-    moment().format();
-    var randVar = Math.floor(Math.random()*(1758 - 1) + 1);
-    if(randVar ==404) /** XKCD doesn't have a comic #404. Top kek XKCD. **/
-    {randVar = Math.floor(Math.random()*(1758 - 1) + 1);}
-        if(Math.random() >= 0.5) /**Randomly chooses between XKCD and C&H**/
-        {
-            min = Math.ceil(0);
-            max = Math.floor(4462);
+function Memes(client, msg, args)   {
 
-            msg.reply('http://explosm.net/comics/' + (Math.floor(Math.random()* max) + 1));
-        }
-        else
-        {
-            var xkcd = require('xkcd');
-            xkcd(randVar, function (data)
-            {
-                msg.channel.sendMessage(data.title + '\n' + moment().month(data.month-1).format("MMMM") + " " + data.day + ", " + data.year + "\n" +  data.img);
-            });
-        }
+    /**Randomly chooses between XKCD and C&H**/
+    if(Math.random() >= 0.5)    {
+        const max = 4462;
+        msg.reply('http://explosm.net/comics/' + (Math.floor(Math.random()* max) + 1));
+    }   else    {
+        var randVar = Math.floor(Math.random()*(1758 - 1) + 1);
+
+        /** XKCD doesn't have a comic #404. Top kek XKCD. **/
+        if(randVar ==404) randVar = Math.floor(Math.random()*(1758 - 1) + 1);
+
+        var xkcd = require('xkcd');
+        xkcd(randVar, function (data)   {
+            msg.channel.sendMessage(data.title + '\n' + moment().month(data.month-1).format("MMMM") + " " + data.day + ", " + data.year + "\n" +  data.img);
+        });
+    }
 }
 module.exports = Memes;
