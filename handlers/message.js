32c1e8414cef4bd2ab0dfd32241c329b235b9550
appellation <nelson.will@live.com>
2016-12-16 21:07:28 -0800

    update validator, add command logging, void errors

--- handlers/message.js
+++ handlers/message.js
@@ -2,18 +2,32 @@
  * Created by Will on 12/6/2016.
  */
 
+const rp = require('request-promise-native');
 const command = require('discord-handles')({
     respond: true,
     directory: __dirname + '/../commands',
     validator: message => {
         const regex = new RegExp(`^<@!?${process.env.discord_client_id}> *`);
-
-        if(message.channel.name === 'pleb' || regex.test(message.content)) return message.content.replace(regex, '');
+        if(message.channel.name === 'pleb' || regex.test(message.content) || message.channel.type === 'dm') return message.content.replace(regex, '');
     }
 });
 
+if(process.env.ifttt)   {
+    command().on('commandStarted', obj => {
+        console.log(obj);
+        rp.post('https://maker.ifttt.com/trigger/pleb/with/key/' + process.env.ifttt, {
+            body: {
+                value1: obj.content,
+                value2: obj.message.author.id,
+                value3: obj.message.guild.id
+            },
+            json: true
+        }).catch(() => null);
+    });
+}
+
 function message(message, body)   {
-    command(message, body).catch(console.error);
+    command(message, body).catch(() => null);
 }
 
 module.exports = message;
