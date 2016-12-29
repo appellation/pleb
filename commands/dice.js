8d202ddabd681047a32b399c0e0439dac6ac69b8
ItsJordan <itzjordanyt@hotmail.com>
2016-12-28 22:42:10 -0500

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -6,12 +6,10 @@ exports.func = (msg, args, handler, options = {}) => {
     const count = parseInt(options.coinflip ? 1 : (args[0] || 2));
     const sides = parseInt(options.coinflip ? 2 : (args[1] || 6));
 
-    if(count >= 1000)  { return 'please use less than 1k dice.  thx fam.'; }
+    if(count >= 1000)  { return 'Please use less than 1,000 dice.  kthx fam.'; }
 
     let sum = 0;
-    for(let i = 0; i < count; i++)  {
-        sum += Math.floor(Math.random() * sides) + 1;
-    }
+    for(let i = 0; i < count; i++)  { sum += Math.floor(Math.random() * sides) + 1; }
 
     if(options.coinflip) return (sum === 1) ? 'heads' : 'tails';
     return `🎲 **${sum}**`;
