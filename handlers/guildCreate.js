d2b8755e1e3d05bfacce7d6e20fc2fa8b57e2765
ItsJordan <itzjordanyt@hotmail.com>
2016-12-29 23:42:53 -0500

    ¯\_(ツ)_/¯

--- handlers/guildCreate.js
+++ handlers/guildCreate.js
@@ -2,8 +2,6 @@
  * Created by Will on 12/6/2016.
  */
 
-function guildCreate(guild) {
-    guild.defaultChannel.sendMessage('Sup.  Try `@Pleb help`.');
-}
+function guildCreate(guild) { guild.defaultChannel.sendMessage('Sup.  Try `@Pleb help`.'); }
 
 module.exports = guildCreate;
