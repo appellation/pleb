d47a5eaa3bdf912716398da2b4c00b0d05c27105
appellation <nelson.will@live.com>
2017-01-29 10:15:55 -0800

    properly destroy playlists on disconnect

--- src/handlers/voiceStateUpdate.js
+++ src/handlers/voiceStateUpdate.js
@@ -2,12 +2,11 @@
  * Created by Will on 12/6/2016.
  */
 
+const storage = require('../util/storage/playlists');
+
 function voiceStateUpdate(oldM, newM)   {
-    if(oldM.user.id !== oldM.client.user.id || oldM.guild.id !== newM.guild.id || newM.voiceChannel) return;
-    if(oldM.guild.playlist) {
-        oldM.guild.playlist.destroy();
-        delete oldM.guild.playlist;
-    }
+    if(oldM.user.id !== oldM.client.user.id || oldM.guild.id !== newM.guild.id || newM.voiceChannel || !storage.has(oldM.guild.id)) return;
+    storage.get(oldM.guild.id).destroy();
 }
 
 module.exports = voiceStateUpdate;
