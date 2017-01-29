fa1a4dbb4bb4601d36ccf2441dd3a93a802621b5
appellation <nelson.will@live.com>
2017-01-29 10:31:48 -0800

    derp

--- src/handlers/voiceStateUpdate.js
+++ src/handlers/voiceStateUpdate.js
@@ -5,8 +5,8 @@
 const storage = require('../util/storage/playlists');
 
 function voiceStateUpdate(oldM, newM)   {
-    if(oldM.user.id !== oldM.client.user.id || oldM.guild.id !== newM.guild.id || newM.voiceChannel || !storage.has(oldM.guild.id)) return;
-    storage.get(oldM.guild.id).destroy();
+    if(oldM.user.id !== oldM.client.user.id || oldM.guild.id !== newM.guild.id || newM.voiceChannel) return;
+    if(storage.has(oldM.guild.id)) storage.get(oldM.guild.id).destroy();
 }
 
 module.exports = voiceStateUpdate;
