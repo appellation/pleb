3f6d48ffd6a85b32c8a6d07a11d398858144d364
appellation <nelson.will@live.com>
2017-02-22 16:58:36 -0800

    fix loop

--- src/commands/loop.js
+++ src/commands/loop.js
@@ -2,20 +2,17 @@
  * Created by Will on 12/22/2016.
  */
 
-// const storage = require('../storage/playlists');
-const Playlist = require('./playlist');
-const VC = require('../util/audio/voiceConnection');
-const Play = require('./play');
+const Playlist = require('../util/audio/Playlist');
+const Operator = require('../util/audio/PlaylistOperator');
 
-exports.func = (res, msg, args, handler) => {
-    let playlist;
-    return VC.checkCurrent(msg.client, msg).then(conn => {
-        playlist = new Playlist(conn);
-        return playlist.add(args.join(' '));
-    }).then(list => {
-        for(let i = 0; i < 19; i++) list.add(list.list[0]);
-        return Play.func(res, msg, [], handler, {playlistIn: playlist});
-    });
+exports.func = (res, msg, args) => {
+    const pl = new Playlist();
+    return pl.add(args).then(list => Operator.init(msg.member, list))
+        .then(op => {
+            const first = op.playlist.list;
+            for(let i = 1; i < parseInt(args[0]) || 20; i++) op.playlist.list = op.playlist.list.concat(first);
+            op.start(res);
+        });
 };
 
-exports.validator = msg => msg.ensureGuild(msg);
+exports.validator = (val, cmd) => val.ensureGuild() && val.applyValid(isNaN(cmd.args[0]) ? true : parseInt(cmd.args[0]) < 50, 'Cannot loop more 50 or more times.');
