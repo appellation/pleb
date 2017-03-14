905b12a0b354b183b8c727385dd736ecdcfb45d0
appellation <nelson.will@live.com>
2017-03-13 21:58:47 -0700

    rip

--- src/commands/sanitize.js
+++ src/commands/sanitize.js
@@ -7,7 +7,7 @@ exports.func = async (res, msg, args) => {
     if(isNaN(num)) return;
 
     const collection = await msg.channel.fetchMessages();
-    let messages = collection.array().filter(m => m.author.id === msg.client.id).slice(0, num);
+    let messages = collection.array().filter(m => m.author.id === msg.client.user.id).slice(0, num);
     if(messages.length < 1) return res.error('Unable to find any messages to purge.');
     if(messages.length === 1) {
         await messages[0].delete();
