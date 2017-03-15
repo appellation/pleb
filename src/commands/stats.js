76068fba70661ee0932e7f363557523a369b53db
Will Nelson <nelson.will@live.com>
2017-03-15 13:33:45 -0700

    sharding stats

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -9,12 +9,23 @@ const playlistStorage = require('../util/storage/playlists');
 exports.func = async (res, msg) => {
     const client = msg.client;
 
+    const stats = {};
+    stats.servers = await client.shard.broadcastEval('this.guilds.size');
+    stats.channels = await client.shard.broadcastEval('this.channels.size');
+    stats.users = await client.shard.broadcastEval('this.guilds.reduce((p, c) => p + c.memberCount, 0)');
+    stats.playlists = await client.shard.broadcastEval(
+        'const path = require(\'path\');\
+        require(path.join(process.cwd(), \'src\', \'util\', \'storage\', \'playlists\')).size;'
+    );
+
+    for(const s in stats) if(stats.hasOwnProperty(s)) stats[s] = stats[s].reduce((a, b) => a + b);
+
     if(process.env.discord_pw) {
         request({
             uri: `https://bots.discord.pw/api/bots/${process.env.discord_client_id}/stats`,
             method: 'post',
             body: {
-                server_count: client.guilds.size
+                server_count: stats.servers
             },
             headers: {
                 Authorization: process.env.discord_pw
@@ -23,9 +34,16 @@ exports.func = async (res, msg) => {
         });
     }
 
-    return res.send(`**Guilds:** ${client.guilds.size}
+    return res.send(`**Guilds:** ${stats.servers}
+**Channels:** ${stats.channels}
+**Users:** ${stats.users}
+**Playlists:** ${stats.playlists}
+
+__**Shard info:**__
+**Shard:** ${client.shard.id + 1} / ${client.shard.count}
+**Guilds:** ${client.guilds.size}
 **Channels:** ${client.channels.size}
-**Users:** ${client.users.size}
+**Users:** ${client.guilds.reduce((p, c) => p + c.memberCount, 0)}
 **Playlists:** ${playlistStorage.size}
 
 __**Process info:**__
