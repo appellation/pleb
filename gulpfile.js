b7432fc3f5ae0b8d20abce66e6e91d7d7793e2e1
appellation <nelson.will@live.com>
2017-03-23 20:30:39 -0700

    basic site

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+
+gulp.task('default', ['css']);
+gulp.watch('site/src/**/*', ['css']);
+
+gulp.task('css', () => {
+    gulp.src('site/src/styles/**')
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('site/public'));
+});
---
+++ site/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+process.env.NODE_ENV = 'development';
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.disable('view cache');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
