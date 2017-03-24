efc6eea6d20e300048e90b50287924e2c0c689cc
appellation <nelson.will@live.com>
2017-03-24 00:42:54 -0700

    bulma

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
@@ -6,8 +7,10 @@ gulp.task('default', ['css']);
 gulp.watch('site/src/**/*', ['css']);
 
 gulp.task('css', () => {
-    gulp.src('site/src/styles/**')
-        .pipe(sass())
+    gulp.src('site/src/styles/main.sass')
+        .pipe(sass({ includePaths: [
+            path.join(process.cwd(), 'node_modules')
+        ]}))
         .pipe(cleanCSS())
         .pipe(gulp.dest('site/bin'));
 });
