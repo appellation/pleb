886271e9271f2c4dbb5501e21e619bc043dc8925
appellation <nelson.will@live.com>
2017-04-05 17:18:11 -0700

    derp

--- src/handlers/disconnect.js
+++ src/handlers/disconnect.js
@@ -1,5 +1,5 @@
 module.exports = (client, close) => {
-    this.client.log.hook({
+    client.log.hook({
         title: `Shard \`${client.shard.id}\` disconnected with code **${close.code}**`,
         color: 0xf44242
     });
