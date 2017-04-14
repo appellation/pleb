245f9c5ac83ab5ecfebe9068186cae43a62f66a1
Will Nelson <nelson.will@live.com>
2017-04-13 18:03:39 -0700

    just die already

--- src/handlers/disconnect.js
+++ src/handlers/disconnect.js
@@ -1,14 +1,3 @@
-let disconnects = 0;
-
-setTimeout(() => {
-    if(disconnects > 0) disconnects--;
-}, 300000); // 5 minutes
-
-module.exports = async (client, close) => {
-    await client.log.hook({
-        title: `Disconnected with code **${close.code}**`,
-        color: 0xf44242
-    });
-
-    if(++disconnects > 3) process.exit(0);
+module.exports = () => {
+    process.exit(0);
 };
