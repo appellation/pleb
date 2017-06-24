a6f7ef5b7c5874ca90b6f886e7b7fde030962c47
KyeNormanGill <kye.normangill@tafensw.net.au>
2017-06-25 06:35:34 +1000

    Weather Refactor

--- src/commands/util/weather.js
+++ src/commands/util/weather.js
@@ -44,42 +44,22 @@ exports.exec = async ({ response, message: msg, args }) => {
   const city = locality || governing || country || continent || {};
 
   const weatherRes = (await request(weatherOptions)).data;
-  Canvas.registerFont(path.join(__dirname, '..', '..', '..', 'assets', 'fonts', 'NotoSans-Regular.ttf'), { family: 'NotoSans' });
-  Canvas.registerFont(path.join(__dirname, '..', '..', '..', 'assets', 'fonts', 'NotoSans-Bold.ttf'), { family: 'NotoSansBold' });
 
-  const images = path.join(__dirname, '..', '..', '..', 'assets', 'images', 'weather');
+  Canvas.registerFont(path.resolve(__dirname, '..', '..', '..', 'assets', 'fonts', 'NotoSans-Regular.ttf'), { family: 'NotoSans' });
+  Canvas.registerFont(path.resolve(__dirname, '..', '..', '..', 'assets', 'fonts', 'NotoSans-Bold.ttf'), { family: 'NotoSansBold' });
+
+  const images = path.resolve(__dirname, '..', '..', '..', 'assets', 'images', 'weather');
 
   const canvas = new Canvas(800, 250);
   const ctx = canvas.getContext('2d');
+
   const Image = Canvas.Image;
+
   const icon = new Image();
   const background = new Image();
+
   icon.src = path.resolve(images, `${getIcon(weatherRes.currently.icon)}.png`);
   background.src = path.resolve(images, 'weatherbg.png');
-
-  const day1Time = timezone().tz(weatherRes.timezone).add(1, 'days').format('ddd');
-  const day1TempMin = convertFToC(weatherRes.daily.data[0].temperatureMin);
-  const day1TempMax = convertFToC(weatherRes.daily.data[0].temperatureMax);
-  const day1Icon = new Image();
-  day1Icon.src = path.resolve(images, `${getIcon(weatherRes.daily.data[0].icon)}.png`);
-
-  const day2Time = timezone().tz(weatherRes.timezone).add(2, 'days').format('ddd');
-  const day2TempMin = convertFToC(weatherRes.daily.data[1].temperatureMin);
-  const day2TempMax = convertFToC(weatherRes.daily.data[1].temperatureMax);
-  const day2Icon = new Image();
-  day2Icon.src = path.resolve(images, `${getIcon(weatherRes.daily.data[1].icon)}.png`);
-
-  const day3Time = timezone().tz(weatherRes.timezone).add(3, 'days').format('ddd');
-  const day3TempMin = convertFToC(weatherRes.daily.data[2].temperatureMin);
-  const day3TempMax = convertFToC(weatherRes.daily.data[2].temperatureMax);
-  const day3Icon = new Image();
-  day3Icon.src = path.resolve(images, `${getIcon(weatherRes.daily.data[2].icon)}.png`);
-
-  const day4Time = timezone().tz(weatherRes.timezone).add(4, 'days').format('ddd');
-  const day4TempMin = convertFToC(weatherRes.daily.data[3].temperatureMin);
-  const day4TempMax = convertFToC(weatherRes.daily.data[3].temperatureMax);
-  const day4Icon = new Image();
-  day4Icon.src = path.resolve(images, `${getIcon(weatherRes.daily.data[3].icon)}.png`);
 
   ctx.drawImage(background, 0, 0);
 
@@ -94,60 +74,38 @@ exports.exec = async ({ response, message: msg, args }) => {
   ctx.font='70px NotoSans';
   ctx.fillText(`${convertFToC(weatherRes.currently.temperature)}°`, 80, 205);
 
-  ctx.textAlign = 'center';
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day1Time, 365, 60);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day2Time, 475, 60);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day3Time, 595, 60);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day4Time, 705, 60);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day1TempMin, 365, 170);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day1TempMax, 365, 210);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day2TempMin, 475, 170);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day2TempMax, 475, 210);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day3TempMin, 595, 170);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day3TempMax, 595, 210);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day4TempMin, 705, 170);
-
-  ctx.fillStyle = '#ffffff';
-  ctx.font='25px NotoSans';
-  ctx.fillText(day4TempMax, 705, 210);
-
-  ctx.drawImage(day1Icon, 330, 70, 60, 60);
-  ctx.drawImage(day2Icon, 440, 70, 60, 60);
-  ctx.drawImage(day3Icon, 560, 70, 60, 60);
-  ctx.drawImage(day3Icon, 670, 70, 60, 60);
   ctx.drawImage(icon, 80, 40, 90, 90);
+
+  const timePlacements = [[365, 60], [475, 60], [595, 60], [705, 60]];
+
+  const minPlacements = [[365, 170], [475, 170], [595, 170], [705, 170]];
+
+  const maxPlacements = [[365, 210], [475, 210], [595, 210], [705, 210]];
+
+  const imagePlacements = [[330, 70], [440, 70], [560, 70], [670, 70]];
+
+  for (let i = 0; i < 4; i++) {
+    const Time = timezone().tz(weatherRes.timezone).add(parseInt(i) + 1, 'days').format('ddd');
+    const Min = convertFToC(weatherRes.daily.data[i].temperatureMin);
+    const Max = convertFToC(weatherRes.daily.data[i].temperatureMax);
+
+    const newImage = new Image();
+    newImage.src = path.resolve(__dirname, '..', '..', '..', 'assets', 'images', 'weather', `${getIcon(weatherRes.daily.data[i].icon)}.png`);
+    ctx.drawImage(newImage, imagePlacements[i][0], imagePlacements[i][1], 60, 60);
+
+    ctx.textAlign = 'center';
+    ctx.fillStyle = '#ffffff';
+    ctx.font = '25px NotoSans';
+    ctx.fillText(Time, timePlacements[i][0], timePlacements[i][1]);
+
+    ctx.fillStyle = '#ffffff';
+    ctx.font = '25px NotoSans';
+    ctx.fillText(Min, minPlacements[i][0], minPlacements[i][1]);
+
+    ctx.fillStyle = '#ffffff';
+    ctx.font = '25px NotoSans';
+    ctx.fillText(Max, maxPlacements[i][0], maxPlacements[i][1]);
+  }
 
   return msg.channel.send(void 0, { files: [{ attachment: canvas.toBuffer() }] });
 };
