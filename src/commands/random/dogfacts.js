1d8258fa8b0841e01307853251722e2c379f3c57
Will Nelson <nelson.will@live.com>
2017-08-30 22:18:29 -0700

    fix async/await

--- src/commands/random/dogfacts.js
+++ src/commands/random/dogfacts.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-exports.exec = (cmd) => {
+exports.exec = await (cmd) => {
   const fact = await axios.get('https://dog-api.kinduff.com/api/facts');
   if (fact.data.success) cmd.response.success(fact.data.facts[0]);
   else cmd.response.error('error fetching a dog fact');
