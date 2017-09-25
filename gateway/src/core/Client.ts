4aaa2f58d2e4ae4c6593dc204ddc66890777f3f6
Will Nelson <nelson.will@live.com>
2017-09-25 15:59:20 -0700

    cleanup client

--- gateway/src/core/Client.ts
+++ gateway/src/core/Client.ts
@@ -6,13 +6,8 @@ import { Error, codes } from '../util/errors';
 export type Gateway = { url: string, shards: number } | null;
 
 export default class Client {
-  public readonly connections: Connection[];
-  public gateway: Gateway;
-
-  constructor() {
-    this.connections = [];
-    this.gateway = null;
-  }
+  public readonly connections: Connection[] = [];
+  public gateway: Gateway = null;
 
   async fetchGateway(): Promise<Gateway> {
     return this.gateway = (await request.get('/gateway/bot')).data;
@@ -20,6 +15,8 @@ export default class Client {
 
   spawn() {
     if (!this.gateway) throw new Error(codes.NO_GATEWAY);
+
+    this.connections.splice(0, this.connections.length);
     for (let i = 0; i < this.gateway.shards; i++) {
       const conn = new Connection(this, i);
       this.connections.push(conn);
