8df04132e829c1c619a10ba5f60f80d765dd26c1
Will Nelson <nelson.will@live.com>
2017-10-10 09:28:14 -0700

    fix user resolver

--- bot/src/util/resolvers.js
+++ bot/src/util/resolvers.js
@@ -1,4 +1,4 @@
 const { MessageMentions } = require('discord.js');
 
-exports.user = c => MessageMentions.USERS_PATTERN.test(c) ? c.match(MessageMentions.USERS_PATTERN)[0] : null;
+exports.user = (c, m) => MessageMentions.USERS_PATTERN.test(c) ? m.client.users.get(c.match(MessageMentions.USERS_PATTERN)[0]) : null;
 exports.integer = c => !c || isNaN(c) ? null : parseInt(c);
