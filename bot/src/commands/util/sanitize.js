eff398f7eb13be167d0dad59b57d20133a112631
Will Nelson <nelson.will@live.com>
2017-10-26 14:27:03 -0700

    make sanitize purge from all authors

--- bot/src/commands/util/sanitize.js
+++ bot/src/commands/util/sanitize.js
@@ -1,5 +1,5 @@
 const resolvers = require('../../util/resolvers');
-const { Argument, Command } = require('discord-handles');
+const { Argument, Command, Validator } = require('discord-handles');
 
 module.exports = class extends Command {
   static get triggers() {
@@ -7,6 +7,8 @@ module.exports = class extends Command {
   }
 
   async pre() {
+    await new Validator(this).ensureClientPermissions('MANAGE_MESSAGES');
+
     await new Argument(this, 'count')
       .setRePrompt('Please provide a valid number of messages to sanitize.')
       .setOptional()
@@ -15,14 +17,13 @@ module.exports = class extends Command {
 
   async exec() {
     const collection = await this.channel.fetchMessages();
-    const messages = collection.array().filter(m => m.author.id === this.client.user.id).slice(0, this.args.count || 3);
+    const messages = collection.first(Math.min(this.args.count || 3, 100));
     if (messages.length < 1) return this.response.error('Unable to find any messages to purge.');
     if (messages.length === 1) {
       await messages[0].delete();
       return this.response.success('Purged last message.', this.author);
     }
-    if (!this.channel.permissionsFor(this.client.user).has('MANAGE_MESSAGES')) return this.response.error('I need `Manage Messages` permissions to sanitize.');
     const deleted = await this.channel.bulkDelete(messages);
-    return this.response.success(`Purged last ${deleted.size} messages.`, this.author);
+    return this.response.success(`Purged last ${deleted.size} messages.`);
   }
 };
