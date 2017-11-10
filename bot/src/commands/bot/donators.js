d63cde830de694043ce44be4dfa68d6519f2607a
Will Nelson <nelson.will@live.com>
2017-11-10 15:43:33 -0800

    update donate command

--- bot/src/commands/bot/donators.js
+++
@@ -1,7 +0,0 @@
-const { Command } = require('discord-handles');
-
-module.exports = class extends Command {
-  exec() {
-    return this.response.send('Special thanks to:\n- **Moofy#9177**\n- **ItsJojj#6161**\n- **Nomsy#7453**');
-  }
-};
