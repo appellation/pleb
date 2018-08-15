60fd79b85c612b429c56f679c025ba5b34aa46ee
Will <nelson.will@live.com>
2018-08-14 21:43:32 -0700

    fix sanitize command

--- bot/src/commands/util/sanitize.js
+++ bot/src/commands/util/sanitize.js
@@ -19,7 +19,7 @@ module.exports = class extends Command {
 
   async exec() {
     const count = Math.min(this.args.count || 3, 100);
-    const messages = await this.channel.messages.fetch({ limit: count });
+    const messages = await this.channel.fetchMessages({ limit: count });
     if (messages.length < 1) return this.response.error('Unable to find any messages to purge.');
     if (messages.length === 1) {
       await messages[0].delete();
